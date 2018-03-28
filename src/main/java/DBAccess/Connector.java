d4e3540d1bee45a4969f48a5e82ee902d43d2bf5
Dorte <Dorte@192.168.0.5>
2018-03-28 22:20:00 +0200

    final

---
+++ src/main/java/DBAccess/Connector.java
@@ -0,0 +1,34 @@
+package DBAccess;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ The purpose of Connector is to...
+
+ @author kasper
+ */
+public class Connector {
+
+   // private static final String URL = "jdbc:mysql://46.101.253.149:3306/useradmin";
+   private static final String URL = "jdbc:mysql://167.99.134.129:3306/useradmin?serverTimezone=UTC";
+   // private static final String URL = "jdbc:mysql://localhost:3306/useradmin?serverTimezone=UTC";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "test";
+
+    private static Connection singleton;
+
+    public static void setConnection( Connection con ) {
+        singleton = con;
+    }
+
+    public static Connection connection() throws ClassNotFoundException, SQLException {
+        if ( singleton == null ) {
+            Class.forName( "com.mysql.cj.jdbc.Driver" );
+            singleton = DriverManager.getConnection( URL, USERNAME, PASSWORD );
+        }
+        return singleton;
+    }
+
+}
